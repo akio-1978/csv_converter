042b907f247008a5fde7e52381760104954a0a0f
akio-1978 <akio.ogawa01@gmail.com>
2021-02-09 15:05:33 +0900

    コンストラクタがjinjaのテンプレートを生成するところ

--- formatchanger.py
+++ formatchanger.py
@@ -1,7 +1,15 @@
+import sys
 from jinja2 import Template, Environment, FileSystemLoader
 
 class FormatChanger:
 
     def __init__(self, *, template):
         self.template = template
+        environment = Environment(loader = FileSystemLoader('./templates', encoding='utf-8'))
+        self.jinja_template = environment.get_template(self.template)
     
+
+if __name__ == '__main__':
+    template_file = sys.argv[1]
+    format_changer = FormatChanger(template = template_file)
+    print(template_file, 'ok.')
