26d764464ffab7cad7c9d08eceff34a3845049a7
akio-1978 <akio.ogawa01@gmail.com>
2021-02-26 17:21:23 +0900

    とりあえずこんな形か？バージョン

--- csv_transformer.py
+++ csv_transformer.py
@@ -1,3 +1,4 @@
+import io
 import sys
 from pathlib import Path
 from jinja2 import Template, Environment, FileSystemLoader
@@ -10,32 +11,42 @@ class CsvTransformer:
         self.template_file = path.name
         environment = Environment(loader = FileSystemLoader(self.loader_directory, encoding='utf-8'))
         self.template = environment.get_template(self.template_file)
-    
-    def execute(self, *, file):
+
+    def transform(self, *, file):
         lines = []
         with open(file, 'r', encoding='utf-8') as source:
             for line in source:
-                lines.append(self.transform_line(line = line.split(',')))
+                lines.append(self.transform_columns(columns = self.parse_tokens(line = line)))
         print(
             self.template.render(
                 {'lines' : lines}
             )
         )
 
-    def transform_line(self, *, line):
-        line_no = 0
-        result = {}
-        for token in line:
-            result[self.column_name(name = None, column_no = line_no)] = str(token).strip()
-            line_no = line_no + 1
-        result['!LINE_NUMBER'] = line_no
-        return result
-
-    def column_name(self, *, name = None, column_no):
+    def column_name(self, *, column_no):
         # 'column_01'とかの文字列
         return 'column_' + str(column_no).zfill(2)
 
+    def parse_tokens(self, *, line):
+        return line.split(',')
+
+    def transform_columns(self, *, columns):
+        line_no = 0
+        result = {}
+        for column in columns:
+            column_name = self.column_name(column_no = line_no)
+            result[column_name] = self.column(token = column)
+            line_no = line_no + 1
+        return result
+
+    def column(self, *, token, line_no=None, column_name=None):
+        return token.strip()
+
+
 if __name__ == '__main__':
+    # windows対策
+    sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
+
     template_file = sys.argv[1]
     format_changer = CsvTransformer(template = template_file)
-    format_changer.execute(file = sys.argv[2])
+    format_changer.transform(file = sys.argv[2])
