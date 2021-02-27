dc987b602bb05bc34ed753b96c20c2620855923d
akio-1978 <akio.ogawa01@gmail.com>
2021-02-27 17:12:30 +0900

    コメントの追加

--- csv_transformer.py
+++ csv_transformer.py
@@ -5,6 +5,7 @@ from jinja2 import Template, Environment, FileSystemLoader
 
 class CsvTransformer:
 
+    # jinja2テンプレートの生成
     def __init__(self, *, template):
         path = Path(template)
         self.loader_directory = path.parent
@@ -12,6 +13,7 @@ class CsvTransformer:
         environment = Environment(loader = FileSystemLoader(self.loader_directory, encoding='utf-8'))
         self.template = environment.get_template(self.template_file)
 
+    # CSVファイルの各行にテンプレートを適用して、出力する
     def transform(self, *, file):
         lines = []
         with open(file, 'r', encoding='utf-8') as source:
@@ -23,13 +25,15 @@ class CsvTransformer:
             )
         )
 
-    def column_name(self, *, column_no):
-        # 'column_01'とかの文字列
-        return 'column_' + str(column_no).zfill(2)
+    # 以下はオーバーライドできるようにメソッドを細かく分けた
 
+    # 行をカラムに分解
     def parse_tokens(self, *, line):
+        # 今回は単純なsplitを使う
+        # エスケープの必要なcsvはテンプレートの適用も大変なので、単純に
         return line.split(',')
 
+    # 1行分の分解されたカラムを処理する
     def transform_columns(self, *, columns):
         line_no = 0
         result = {}
@@ -37,10 +41,23 @@ class CsvTransformer:
             column_name = self.column_name(column_no = line_no)
             result[column_name] = self.column(token = column)
             line_no = line_no + 1
-        return result
+        return self.transformed_line(result = result)
 
+    # CSVのカラムに名前を付ける
+    # このカラム名がテンプレートから参照される
+    def column_name(self, *, column_no):
+        # 'column_01'とかの文字列
+        return 'column_' + str(column_no).zfill(2)
+
+    # 分解されたひとつのカラムの処理
     def column(self, *, token, line_no=None, column_name=None):
+        # stripだけ
         return token.strip()
+
+    # 1行分の処理結果
+    def transformed_line(self, *, result):
+        # 今回はそのまま返す
+        return result
 
 
 if __name__ == '__main__':
