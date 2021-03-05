7ef34ca229aeecfff45040f0312ac7d0787a18e5
akio-1978 <akio.ogawa01@gmail.com>
2021-03-05 11:23:35 +0900

    強引だがここからPoetryに移行できるかテスト前のcommit

--- csvtransformer/argument_parser.py
+++ csvtransformer/argument_parser.py
@@ -14,31 +14,50 @@ class TransformArgumentParser():
         # jinja2 template to use.
         parser.add_argument('template', help='jinja2 template to use.')
         parser.add_argument('csv', help='transform csv.')
-        parser.add_argument('additional', nargs='*', help='additional values [KEY=VALUE] format.')
-
+        parser.add_argument('key_values', nargs='*', help='additional values [KEY=VALUE] format.', action=KeyValuesParseAction)
         # flag first line is header
         parser.add_argument('-H', '--header', help='first line is header.', action='store_true')
         # flag tab separate values
-        parser.add_argument('-T', '--tab', help='tab separate values.', action='store_true')
+        parser.add_argument('-T', '--tab', help='tab separate values.', dest='delimiter', default=',', action=DelimiterSelectAction)
         # source encoding
-        parser.add_argument('-E', '--encoding', metavar='enc', help='file encoding.', nargs=1, default='utf-8')
+        parser.add_argument('--input-encoding', metavar='enc', help='file encoding.', default='utf-8')
         # output file (default stdout)
         parser.add_argument('-O', '--output', metavar='file', help='output file.', nargs=1)
 
         options = TransfomerParameters()
         parser.parse_args(args=args, namespace=options)
-        options.additional = self.parse_addtional(options.additional)
 
         return options
 
-    def parse_addtional(self, values):
-        addtional = {}
+    # def parse_addtional(self, values):
+    #     addtional = {}
+    #     for value in values:
+    #         key_value = value.split('=')
+    #         addtional[key_value[0]] = key_value[1]
+    #     return addtional
+
+class KeyValuesParseAction(argparse.Action):
+
+    def __call__(self, parser, namespace, values, option_string=None):
+        setattr(namespace, self.dest, self.parse_key_values(values))
+
+    def parse_key_values(self, values):
+        key_values = {}
         for value in values:
             key_value = value.split('=')
-            addtional[key_value[0]] = key_value[1]
-        return addtional
+            key_values[key_value[0]] = key_value[1]
+        return key_values
 
+class DelimiterSelectAction(argparse.Action):
 
+    def __call__(self, parser, namespace, values, option_string):
+        print('call action.')
+
+        delimiter = ''
+        if option_string == '-T' or option_string == '--tab':
+            delimiter = '\t'
+
+        setattr(namespace, self.dest, delimiter)
 
 if __name__ == '__main__':
     # windows対策
--- test/test_argument_parser.py
+++ test/test_argument_parser.py
@@ -9,6 +9,59 @@ class ArgumentParserTest(unittest.TestCase):
         
         self.assertEqual('template', params.template)
         self.assertEqual('csv', params.csv)
+        self.assertFalse(params.header)
+
+    def test_header(self):
+        params = TransformArgumentParser().parse_parameters(['template', 'csv', '-H'])
+        self.assertEqual('template', params.template)
+        self.assertEqual('csv', params.csv)
+        self.assertTrue(params.header)
+
+        params = TransformArgumentParser().parse_parameters(['template', 'csv', '--header'])
+        self.assertEqual('template', params.template)
+        self.assertEqual('csv', params.csv)
+        self.assertTrue(params.header)
+
+    def test_encoding(self):
+        params = TransformArgumentParser().parse_parameters(['template', 'csv'])
+        self.assertEqual('template', params.template)
+        self.assertEqual('csv', params.csv)
+        self.assertEqual('utf-8', params.input_encoding)
+
+        params = TransformArgumentParser().parse_parameters(['template', 'csv', '--input-encoding', 'sjis'])
+        self.assertEqual('template', params.template)
+        self.assertEqual('csv', params.csv)
+        self.assertEqual('sjis', params.input_encoding)
+
+    def test_key_values_action(self):
+        params = TransformArgumentParser().parse_parameters(['template', 'csv', 'A=B', 'C=D'])
         
+        self.assertEqual('template', params.template)
+        self.assertEqual('csv', params.csv)
+        self.assertFalse(params.header)
+        self.assertEqual(params.key_values['A'], 'B')
+        self.assertEqual(params.key_values['C'], 'D')
+
+    def test_delimiter_comma_action(self):
+        params = TransformArgumentParser().parse_parameters(['template', 'csv', 'A=B'])
+        
+        self.assertEqual('template', params.template)
+        self.assertEqual('csv', params.csv)
+        self.assertFalse(params.header)
+        self.assertEqual(params.delimiter, ',')
+
+    def test_delimiter_tab_action(self):
+        params = TransformArgumentParser().parse_parameters(['template', 'csv', '-T' , 'A=B'])
+        self.assertEqual('template', params.template)
+        self.assertEqual('csv', params.csv)
+        self.assertFalse(params.header)
+        self.assertEqual(params.delimiter, '\t')
+
+        params = TransformArgumentParser().parse_parameters(['template', 'csv', '--tab' , 'A=B'])
+        self.assertEqual('template', params.template)
+        self.assertEqual('csv', params.csv)
+        self.assertFalse(params.header)
+        self.assertEqual(params.delimiter, '\t')
+
 if __name__ == '__main__':
     unittest.main()
