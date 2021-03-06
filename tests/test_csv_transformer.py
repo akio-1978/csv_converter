cc4e53ac57f28050e2f2c573faa8eba773cbea2d
akio-1978 <akio.ogawa01@gmail.com>
2021-03-06 23:56:18 +0900

    絶妙に解りやすく、かつプログラムの有用性を示すテストに悩む

--- tests/test_csv_transformer.py
+++ tests/test_csv_transformer.py
@@ -1,5 +1,71 @@
+import unittest
+from csv_transformer import TransfomerParameters
+from csv_transformer import CsvTransformar
 from csv_transformer import __version__
 
 
 def test_version():
     assert __version__ == '0.1.0'
+
+class CsvTransformerTest(unittest.TestCase):
+    headerless_csv = '''\
+G01,SG01, val001,val002,val003
+G01,SG02, val101,val102,val103
+G02,SG02, val201,val202,val203
+'''
+    headered_csv = '''\
+GROUP, SUBGROUP, V1,V2,V3
+G01,SG01, val001,val002,val003
+G01,SG02, val101,val102,val103
+G02,SG02, val201,val202,val203
+'''
+
+    template_to_json = '''\
+{
+    "groups" : [
+{% for line in lines %}
+        {
+            "group" : "{{line.GROUP}}",
+            "subgroups" : [
+                {
+                    "subgroup" : "{{line.SUBGROUP}}",
+                    "V1" : "{{line.V1}}",
+                    "V2" : "{{line.V2}}",
+                    "V3" : "{{line.V3}}"
+                }
+            ]
+        }
+{%end-for%}
+    ]
+}
+'''
+
+    result_json='''/
+    {
+        "groups" : [
+            {
+                "group" : "G01",
+                "subgroups" : [
+                    {
+                        "subgroup" : "SG01",
+                        "V1" : "val001",
+                        "V2" : "val002",
+                        "V3" : "val003"
+                    }
+                    {
+                        "subgroup" : "SG02",
+                        "V1" : "val101",
+                        "V2" : "val102",
+                        "V3" : "val103"
+                    }
+                    {
+                        "subgroup" : "SG03",
+                        "V1" : "val201",
+                        "V2" : "val202",
+                        "V3" : "val203"
+                    }
+                ]
+            },
+        ]
+    }
+'''
