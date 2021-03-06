b2a3e2673fceb8a41b17fe6e6e5edc5353d54722
akio-1978 <akio.ogawa01@gmail.com>
2021-03-06 23:55:31 +0900

    ヘッダの生成、jinja2テンプレートの受入れタイミング追加など

--- csv_transformer/csv_transformer.py
+++ csv_transformer/csv_transformer.py
@@ -4,6 +4,7 @@ import argparse
 from pathlib import Path
 from jinja2 import Template, Environment, FileSystemLoader
 from csv import reader as csvreader
+from csv_transformer import jinja2_filters
 
 class CsvTransformer:
 
@@ -14,20 +15,22 @@ class CsvTransformer:
 
     # オーバーライドすると、ファイル名指定以外の方法でテンプレートを取得できる
     def init_template(self, *, parameters):
-        path = Path(parameters.source)
+        path = Path(parameters.template)
         environment = Environment(loader = FileSystemLoader(path.parent, encoding='utf-8'))
         self.template = environment.get_template(path.name)
 
     # CSVファイルの各行にテンプレートを適用して、出力する
     def transform(self, *, source, output):
         lines = []
-        with open(source, 'r', encoding='utf-8', newline='') as csvfile:
+        with self.get_datasource(source=source) as csvfile:
             reader = csvreader(csvfile)
             firstline = True
             for columns in reader:
                 # ヘッダ設定
-                if self.parameters.header and firstline:
+                if firstline:
+                    self.headers = self.get_headers(parameters = self.parameters, columns = columns)
                     firstline = False
+                if self.parameters.header:
                     continue
                 lines.append(self.transform_columns(columns = columns))
         print(
@@ -37,27 +40,36 @@ class CsvTransformer:
         )
 
     # 以下はオーバーライドできるようにメソッドを細かく分けた
+    # jinja2カスタムテンプレートのインストール
+    def install_jinja2_filters(self, *, environment, parameters):
+        environment.filters['groups'] = jinja2_filters.groups
+        return environment
+
+    def get_headers(self, *, parameters, columns):
+        headers = []
+        for idx in range[len(columns)]:
+            column = column[idx] \
+                if parameters.header else parameters.header + parameters.raw_colmun_prefix.zfill(2)
+            headers.append(idx).strip()
+        return headers
+
+    def get_datasource(self, *, source, input_encoding='utf-8'):
+        return open(source, 'r', encoding=input_encoding, newline='')
 
     # 1行分の分解されたカラムを処理する
     def transform_columns(self, *, columns):
         line_no = 0
         result = {}
         for column in columns:
-            column_name = self.column_name(column_no = line_no)
-            result[column_name] = self.column(token = column)
+            header_name = self.column_name(column_no = line_no)
+            result[header_name] = self.column(value = column)
             line_no = line_no + 1
         return self.transformed_line(result = result)
 
-    # CSVのカラムに名前を付ける
-    # このカラム名がテンプレートから参照される
-    def column_name(self, *, column_no):
-        # 'column_01'とかの文字列
-        return 'column_' + str(column_no).zfill(2)
-
     # 分解されたひとつのカラムの処理
-    def column(self, *, token, line_no=None, column_name=None):
+    def column(self, *, value, line_no=None, column_name=None):
         # stripだけ
-        return token.strip()
+        return value.strip()
 
     # 1行分の処理結果
     def transformed_line(self, *, result):
@@ -71,3 +83,4 @@ class TransfomerParameters:
         self.encoding = 'utf8'
         self.separator = ','
         self.source = None
+        self.raw_colmun_prefix='col_'
