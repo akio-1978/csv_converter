e7eabdce2e8af07f6f5d3f549aac8bc5c1d79fff
akio-1978 <akio.ogawa01@gmail.com>
2021-04-08 05:34:49 +0900

    ここでimportは要らないらしい

--- csv_converter/__init__.py
+++ csv_converter/__init__.py
@@ -1,4 +1,4 @@
 __version__ = '0.1.0'
-from .converter import ConverterContext, CsvConverter
-from .convert import ContextBuilder
+# from .convert import ContextBuilder
+# from .converter import ConverterContext, CsvConverter
 
