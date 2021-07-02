b9d69e6c7af80968f3e486dbed81e426c5009ca9
akio-1978 <akio.ogawa01@gmail.com>
2021-07-02 21:37:25 +0900

    ヘルプメッセージの調整

--- j2shrine/command/csv_command.py
+++ j2shrine/command/csv_command.py
@@ -6,11 +6,11 @@ class CsvCommand(Command):
     def add_arguments(self, subparser):
         csv_command_parser = super().add_arguments(subparser=subparser)
         # flag first line is header
-        csv_command_parser.add_argument('-H', '--header', help='use first line is header.', dest='use_header', action='store_true')
+        csv_command_parser.add_argument('-H', '--header', help='with header.', dest='use_header', action='store_true')
         # flag tab separate values
-        csv_command_parser.add_argument('-d', '--delimiter', metavar='', help='values delimiter.', default=',')
+        csv_command_parser.add_argument('-d', '--delimiter', metavar='', help='column delimiter.', default=',')
         # skip head lines
-        csv_command_parser.add_argument('-s', '--skip-lines', metavar='', help='skip first n lines.', default=0)
+        csv_command_parser.add_argument('-s', '--skip-lines', metavar='', help='skip n lines.', default=0)
         return csv_command_parser
 
     def context(self):
