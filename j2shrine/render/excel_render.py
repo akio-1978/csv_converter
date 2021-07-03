f829dc13e8b0bc3f7af2958004d9b61abbf5c4fa
akio-1978 <akio.ogawa01@gmail.com>
2021-07-03 21:43:30 +0900

    ぐちゃぐちゃで動かないけどまずコミット

--- j2shrine/render/excel_render.py
+++ j2shrine/render/excel_render.py
@@ -1,25 +1,31 @@
-import csv
 import openpyxl
+from openpyxl.worksheet
 from . base_render import Render, RenderContext
 
 # transformerに渡すパラメータクラス
 class ExcelRenderContext(RenderContext):
     def __init__(self, *, template=None, template_encoding='utf8', parameters={}):
         super().__init__(template=template, template_encoding=template_encoding, parameters=parameters)
-        self.use_header = False
         self.encoding = 'utf8'
+        self.sheet_no = 0
         self.header_prefix='col_'
-        self.headers = []
-        self.regions = []
+        self.header_area = None
+        self.headers = None         # Xn:Xn
+        self.start_line = None      # A-Z
+        self.start = '1'
 
 
 class ExcelRender(Render):
 
-    # jinja2テンプレートの生成
-    def __init__(self, *, context):
-        super().__init__(context = context)
+    COLMUN_LABELS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
-    def build_reader(self, *, source):
+    # jinja2テンプレートの生成
+    def __init__(self, *, context :ExcelRenderContext):
+        super().__init__(context = context)
+        if context.headers is not None:
+            self.headers = context.headers
+
+    def build_reader(self, *, source :any) :
         # 既にブックで渡された場合、そのまま返す
         if ( isinstance(source, openpyxl.Workbook)):
             return source
@@ -27,19 +33,42 @@ class ExcelRender(Render):
         return openpyxl.load_workbook(source)
 
     def read_source(self, *, reader):
+
+        # ヘッダの読込み
+        self.headers = self
         lines = []
         for line_no, columns in enumerate(reader):
-            # ヘッダ読み込み、ヘッダがない場合は連番をヘッダにする
-            if line_no == 0:
-                self.headers = self.read_headers(context = self.context, columns = columns)
-                # ヘッダとして先頭行を読み込んだ場合
-                if self.context.use_header:
-                    continue
             
             line = self.columns_to_dict(columns = columns)
             lines.append(line)
 
         return lines
+
+    def read_headers(self, *, sheet:openpyxl.worksheet.workshet):
+        headers = []
+        if self.context.headers is not None:
+            self.headers = self.context.headers
+        elif self.context.header_area is not None:
+            (start_col, start_row, end_col, end_row) = self.region_indice(sheet=sheet, cells=self.context.header_area)
+            row = sheet.iter_rows(min_col=start_col, min_row=start_row,
+                                        max_col=end_col, max_row=end_row)
+
+            for cell in row:
+                headers.append(cell.value)
+            return headers
+        else: # ヘッダは指定されていない
+            return []
+
+    def cell_index(self,*,  sheet, cell_name):
+        cell = sheet[cell_name]
+        return (cell.column, cell.row)
+
+    def region_indice(self, *, sheet, cells:str) :
+        (start_cell, end_cell) = cells.split(':')
+        
+        (start_col, start_row) = self.cell_index(sheet=sheet, cell_name=start_cell)
+        (end_col, end_row) = self.cell_index(sheet=sheet, cell_name=end_cell)
+        return (start_cell, start_row)
 
     # カラムのlistをdictに変換する。dictのキーはself.headers
     def columns_to_dict(self, *, columns):
@@ -54,21 +83,6 @@ class ExcelRender(Render):
 
     def columns_dict(self, *, columns_dict):
         return columns_dict
-
-    def read_headers(self, *, context, columns):
-        headers = []
-        for idx, column in enumerate(columns):
-            header = None
-            if context.use_header:
-                # column value equal header
-                header = column.strip()
-            else:
-                # make header from formatted column position
-                header = context.header_prefix + str(idx).zfill(2)
-
-            headers.append(header)
-        print('header: ', headers)
-        return headers
 
     # hook by every column
     def read_column(self, *, name, column):
