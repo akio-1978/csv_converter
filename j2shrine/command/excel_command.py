7a588cb5d52cfeb49d285ad32136882eaf66f47f
akio-1978 <akio.ogawa01@gmail.com>
2021-08-29 03:24:25 +0900

    ヘルプの整形

--- j2shrine/command/excel_command.py
+++ j2shrine/command/excel_command.py
@@ -4,26 +4,18 @@ from . base_command import Command
 from ..render.excel_render import ExcelRender, ExcelRenderContext
 class ExcelCommand(Command):
 
-    HELP_COLUMNS = """read column letters
-A   read column A only
-A-C read column A B C
-A-  read from column A to last column
+    HELP_COLUMNS = """read columns range (A A-C A-)"""
+    HELP_ROWS = """read row numbers range (1 2-4 1-)"""
+    HELP_SHEETS = """read sheets range (1 1-4 1-)"""
+    HELP_EPILOG = """range arguments format
+  1   read 1 only
+  1-4 read 1 2 3 4
+  1-  read from 1 to last
+  for column, specify letter ex: A, A-C, B-
 """
 
-    # HELP_ROWS = "row read range ex: 2 2-3 2-"
-    HELP_ROWS = """read row numbers
-2   read row 2 only
-2-4 read row 2 3 4
-2-  read from 2 to last row
-"""
-
-    HELP_SHEETS = """read sheets 
-0   read sheets 2 only
-0-2 read sheets 0 1 2
-2-  read from 2 to last sheet
-"""
     def create_parser(self,*, parser_creator):
-        return parser_creator.add_parser('excel', help = 'rendaring excel file', formatter_class=argparse.RawTextHelpFormatter)
+        return parser_creator.add_parser('excel', epilog= ExcelCommand.HELP_EPILOG, help = 'rendaring excel file', formatter_class=argparse.RawTextHelpFormatter)
 
     def add_positional_arguments(self, *, parser):
         parser.add_argument('template', help='jinja2 template to use.')
@@ -33,12 +25,12 @@ A-  read from column A to last column
         parser.add_argument('columns', help=ExcelCommand.HELP_COLUMNS)
         parser.add_argument('rows', help=ExcelCommand.HELP_ROWS)
         # header from sheet rows
-        parser.add_argument('header_row', help='row of use as header. if not specified use column letter.', nargs='?')
+        parser.add_argument('header-row', help='headers row number. default headers use column letter.', nargs='?')
 
     def add_optional_arguments(self, *, parser):
         super().add_optional_arguments(parser=parser)
         # user specified headers
-        parser.add_argument('--headers', help='user specified headers. ex: aaa bbb ccc...', dest='headers', nargs='*')
+        parser.add_argument('--headers', help='direct specified headers. ex: aaa bbb ccc...', dest='headers', nargs='*')
         parser.add_argument('-E', '--extra', help='get fixed position cells. ex: A1 B2...', dest='extra', nargs='*')
 
     def context(self):
