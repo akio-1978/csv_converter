f7387548930ee4898e3e872b4caec64d547122c6
akio-1978 <akio.ogawa01@gmail.com>
2021-10-29 22:15:11 +0900

    テスト内からstdoutのバッファを操作できないのでファイル出力に変えた

--- tests/test_starter.py
+++ tests/test_starter.py
@@ -6,17 +6,15 @@ from j2shrine.starter import Starter
 class StarterTest(unittest.TestCase):
 
     def test_start(self):
-        sys.stdout = io.StringIO()
-        Starter(args=['csv', 'tests/csv/templates/simple_json.tmpl', 'tests/csv/render_source_file/simple_json.csv', '-p' ,'A=B']).execute()
+        Starter(args=['csv', 'tests/csv/templates/simple_json.tmpl', 'tests/csv/render_source_file/simple_json.csv', 
+            '-o', 'tests/output/test_start.tmp', '-p' ,'A=B']).execute()
 
     def test_start_args(self):
-        sys.stdout = io.StringIO()
         Starter(args=['csv', 'tests/csv/templates/simple_json.tmpl', 'tests/csv/render_source_file/simple_json.csv',
-             '-d',',', '-p' ,'A=B']).execute()
+            '-o', 'tests/output/test_start.tmp', '-d',',', '-p' ,'A=B']).execute()
     def test_start_basic_args(self):
-        sys.stdout = io.StringIO()
         Starter(args=['csv', 'tests/csv/templates/simple_json.tmpl', 'tests/csv/render_source_file/simple_json.csv',
-             '--input-encoding','euc-jp', '-p' ,'A=B']).execute()
+            '-o', 'tests/output/test_start.tmp',  '--input-encoding','euc-jp', '-p' ,'A=B']).execute()
 
     def test_excel_help(self):
         starter = Starter(args=['excel', '-h'])
