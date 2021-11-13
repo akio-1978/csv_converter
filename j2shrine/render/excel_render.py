257fc33622e6479ade3f86630953805707b74d17
akio-1978 <akio.ogawa01@gmail.com>
2021-11-14 03:39:34 +0900

    excel風のパラメータ指定に対応

--- j2shrine/render/excel_render.py
+++ j2shrine/render/excel_render.py
@@ -13,13 +13,27 @@ class ExcelRenderContext(RenderContext):
     def __init__(self, *, template=None, template_encoding='utf8', parameters={}):
         super().__init__(template=template, template_encoding=template_encoding, parameters=parameters)
         self.encoding = 'utf8'
-        self.sheets = '0'
+        self.sheets = '1'
         self.header_prefix='col_'
         self.header_row = None     # ex. 1
-        self.rows = '1'      # 1 1-2 1-
-        self.columns = 'A'  # A A-B A-
+        self.read_range = None # A2:C4  (read cells from A2 to C4) or A2:C (read cells in all rows from 2)
         self.limit = None
         self.extra =[]
+
+class  ReadSetting:
+    def __init__(self, *, sheet_left:int, sheet_right:int, left_row:str, left_column:str, right_row:str, right_column:str) -> None:
+        self.sheet_left = sheet_left
+        self.sheet_right = sheet_right
+        self.left_row = left_row
+        self.left_column = left_column
+        self.right_row = right_row
+        self.right_column = right_column
+
+    def get_sheet_right(self, sheets):
+        if self.sheet_right is not None:
+            return self.sheet_right
+        else:
+            return len(sheets.worksheets) -1
 
 class ExcelRender(Render):
 
@@ -37,18 +51,12 @@ class ExcelRender(Render):
         environment.filters['excel_time'] = excel_time
 
     def setup_range(self):
-        (left, right) = self.parse_range(arg_range = self.context.columns)
-        self.left = self.column_number(column=left) if left is not None else 1
-        self.right = self.column_number(column=right) if right is not None else None
 
-        (top, bottom) = self.parse_range(arg_range = self.context.rows)
-        self.top = int(top) if top is not None else 1
-        self.bottom = int(bottom) if bottom is not None else None
+        (left_row, left_column, right_row, right_column) = self.get_read_range(self.context.read_range)
+        (sheet_left, sheet_right) = self.get_sheet_range(self.context.sheets)
 
-        (sheet_left, sheet_right) = self.parse_range(arg_range = self.context.sheets)
-        self.sheet_left = int(sheet_left) - 1
-        self.sheet_right = int(sheet_right) - 1 if sheet_right is not None else None
-
+        return ReadSetting(sheet_left=sheet_left, sheet_right=sheet_right,
+            left_row=left_row, left_column=left_column, right_row=right_row, right_column=right_column)
 
     def build_reader(self, *, source :any) :
         # 既にブックで渡された場合、そのまま返す
@@ -58,15 +66,16 @@ class ExcelRender(Render):
         return openpyxl.load_workbook(source, data_only=True)
 
     def read_source(self, *, reader):
-        # シート取り出し
-        sheet_right = self.sheet_right if self.sheet_right is not None else len(reader.worksheets) -1
-#        print('left:', self.sheet_left, 'to right:', sheet_right)
+
+        setting = self.setup_range()
+        sheet_right = setting.get_sheet_right(sheets=reader.worksheets)
+
         all_sheets = []
-        sheet_idx = self.sheet_left
+        sheet_idx = setting.sheet_left
         while sheet_idx <= sheet_right:
             sheet = reader.worksheets[sheet_idx]
             # ヘッダの読込み
-            headers = self.read_headers(sheet=sheet)
+            headers = self.read_headers(sheet=sheet, setting=setting)
 
             # コンテンツ読込み
             sheet_data = {
@@ -75,8 +84,10 @@ class ExcelRender(Render):
                 'headers' : headers,
                 'extra' : self.read_extra_cells(sheet= sheet)
             }
-            for row in sheet.iter_rows(min_col=self.left, min_row=self.top , max_col=self.right, max_row=self.bottom, ):
+            for row in sheet.iter_rows(min_col=setting.left_column, min_row=setting.left_row , 
+                    max_col=setting.right_column, max_row=setting.right_row, ):
                 sheet_data['rows'].append(self.columns_to_dict(columns = row, headers = headers))
+
             all_sheets.append(sheet_data)
             sheet_idx = 1 + sheet_idx
         return all_sheets
@@ -97,20 +108,20 @@ class ExcelRender(Render):
         return final_result
 
 
-    def read_headers(self, *, sheet:openpyxl.worksheet.worksheet):
+    def read_headers(self, *, sheet:openpyxl.worksheet.worksheet, setting:ReadSetting):
         headers = []
 
         # どちらを通っているかはっきりしていない
         # ヘッダはシートごとの独立させるか否かを検討する
 
         if self.context.header_row is not None:
-            for row in sheet.iter_rows(min_col=self.left, min_row=int(self.context.header_row),
-                                        max_col=self.right, max_row=int(self.context.header_row)):
+            for row in sheet.iter_rows(min_col=setting.left_column, min_row=int(self.context.header_row),
+                                        max_col=setting.right_column, max_row=int(self.context.header_row)):
                 for cell in row:
                     headers.append(cell.value)
         else: 
-            for row in sheet.iter_rows(min_col=self.left, min_row=1,
-                                        max_col=self.right, max_row=1):
+            for row in sheet.iter_rows(min_col=setting.left_column, min_row=1,
+                                        max_col=setting.right_column, max_row=1):
                 for cell in row:
                     headers.append(cell.column_letter)
 
@@ -142,26 +153,34 @@ class ExcelRender(Render):
         number = number + (int(ExcelRender.ALPHABET_TABLE.find(fulldigit[2])))
 
         return number
-    
-    def parse_range(self, *, arg_range:str, all=None):
 
-        if (arg_range is None):
-            return (None, None)
+    def get_sheet_range(self, *, sheets_range:str):
 
-        divided = arg_range.partition('-')
-        if divided[0] == arg_range:
-            # only one
-            return (divided[0], divided[0])
-        if divided[2] != '':
-            # start to end
-            return (divided[0], divided[2])
-        else:
-            # start to all
-            return (divided[0], all)
+        (left, to, right) = sheets_range.partition(':')
+
+        if to == ':':
+            if right == '':
+                return (int(left) -1, None)
+            else:
+                return (int(left) -1, int(right) -1)
+
+        return (int(left) -1, int(left) -1)
+
+    def get_read_range(self, *, arg_range:str, all=None):
+
+        (left, delim, right) = arg_range.partition(':')
+
+        if left == arg_range:
+            raise ValueError('invalid range: ' + arg_range)
+
+        (left_row, left_column) = self.get_coordinate(left)
+        (right_row, right_column) = self.get_coordinate(right)
+
+        return (left_row, left_column, right_row, right_column)
 
     def get_coordinate(self, *, coordinate:str):
-        if coordinate.isalpha():
-            # case in omit row number. read all rows.
+        if coordinate.isalpha() and coordinate.isascii():
+            # args is column only. read all rows.
             column = openpyxl.utils.cell.column_index_from_string(coordinate)
             return (None, column)
         else:
