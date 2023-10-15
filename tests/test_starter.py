ce3bd41d521ca11b42b611344d9a9982478bfef5
akio-1978 <akio.ogawa01@gmail.com>
2023-10-15 17:43:37 +0900

    フォルダ名短縮の影響で失敗したテスト

--- tests/test_starter.py
+++ tests/test_starter.py
@@ -4,18 +4,18 @@ from j2shrine.starter import Starter
 class StarterTest(unittest.TestCase):
 
     def test_start(self):
-        Starter(args=['csv', 'tests/csv/templates/simple_json.tmpl', 'tests/csv/render_source_file/simple_json.csv', 
+        Starter(args=['csv', 'tests/csv/templates/simple_json.tmpl', 'tests/csv/src/simple_json.csv', 
             '-o', 'tests/output/test_start.tmp', '-p' ,'A=B']).execute()
 
     def test_start_args(self):
-        Starter(args=['csv', 'tests/csv/templates/simple_json.tmpl', 'tests/csv/render_source_file/simple_json.csv',
+        Starter(args=['csv', 'tests/csv/templates/simple_json.tmpl', 'tests/csv/src/simple_json.csv',
             '-o', 'tests/output/test_start.tmp', '-d',',', '-p' ,'A=B']).execute()
     def test_start_basic_args(self):
-        Starter(args=['csv', 'tests/csv/templates/simple_json.tmpl', 'tests/csv/render_source_file/simple_json.csv',
+        Starter(args=['csv', 'tests/csv/templates/simple_json.tmpl', 'tests/csv/src/simple_json.csv',
             '-o', 'tests/output/test_start.tmp',  '--input-encoding','euc-jp', '-p' ,'A=B']).execute()
 
     def test_start_excel(self):
-        Starter(args=['excel', 'tests/excel/templates/read_document.tmpl', 'tests/excel/render_source_file/read_document.xlsx',
+        Starter(args=['excel', 'tests/excel/templates/read_document.tmpl', 'tests/excel/src/read_document.xlsx',
             '1:', 'C7:H10', '-o', 'tests/output/test_start.tmp', '--fixed', 'C3', 'C4']).execute()
 
     def test_excel_help(self):
