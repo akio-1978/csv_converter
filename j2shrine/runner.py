94d64d1bdbc1977ee37f7eab40d7a8e91905a865
akio-1978 <akio.ogawa01@gmail.com>
2024-04-24 01:04:41 +0900

    runnerのコメントと型ヒント

--- j2shrine/runner.py
+++ j2shrine/runner.py
@@ -6,36 +6,37 @@ from .json.json_command import JsonCommand
 
 
 class Runner():
+    """コマンドラインから呼び出される処理の起点となるクラス"""
 
-    def __init__(self, *, args):
+    def __init__(self, *, args:list):
         self.args = args
 
-    def set_subparsers(self, *, main_parser):
-        CsvCommand(master=main_parser).setup()
-        ExcelCommand(master=main_parser).setup()
-        JsonCommand(master=main_parser).setup()
-
-    def create_mainparser(self):
-        base_parser = argparse.ArgumentParser(prog='j2shrine',
-                                              add_help=True,
-                                              )
-        return base_parser
+    def register_subcommand(self, *, subparser_factory:argparse.ArgumentParser):
+        """サブコマンドのパーサを構築する"""
+        CsvCommand(master=subparser_factory).setup()
+        ExcelCommand(master=subparser_factory).setup()
+        JsonCommand(master=subparser_factory).setup()
 
     def execute(self):
+        """プログラムの実行本体"""
+        # ArgumentParserの初期化
+        self.parser = argparse.ArgumentParser(prog='j2shrine',
+                                              add_help=True,
+                                              )
+        # ArgumentParserにサブコマンドのパーサーを追加する
+        self.register_subcommand(
+            subparser_factory=self.parser.add_subparsers(required=True))
 
-        self.parser = self.create_mainparser()
-        self.set_subparsers(
-            main_parser=self.parser.add_subparsers(required=True))
-
+        # コマンドライン引数をパースする
         namespace = self.parser.parse_args(self.args)
+        # parse_argsから得られたコマンドをexecuteする
         command = namespace.command_instance
         command.execute(args=namespace)
 
-
 def main():
+    """argparseでsys.argvではなく明示的にリストを受け取るため、リストの長さが足りない場合を補完する"""
     starter = Runner(args=sys.argv[1:] if len(sys.argv) > 1 else ['', ''])
     starter.execute()
-
 
 if __name__ == '__main__':
     main()
