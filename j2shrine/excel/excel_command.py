12c93c47aa7cb826742cea48be4c9c1b8143454d
akio-1978 <akio.ogawa01@gmail.com>
2024-04-29 18:38:47 +0900

    render呼び出しにStreamWrapperを使用

--- j2shrine/excel/excel_command.py
+++ j2shrine/excel/excel_command.py
@@ -5,7 +5,7 @@ from j2shrine.context import RenderContext
 from ..command import Command, KeyValuesParseAction
 from .excel_render import ExcelRender
 from .excel_context import ExcelRenderContext
-
+from ..renderutils import StreamWrapper
 
 class ExcelCommand(Command):
 
@@ -24,6 +24,11 @@ class ExcelCommand(Command):
         return ExcelRenderContext
 
     def add_positional_arguments(self):
+        """excel固有の必須引数があるので、位置引数を定義しなおす
+            * excelブックをstdinから受け取れないため、ファイル名は必須
+            * 読み取り対象シートの指定は必須
+            * 読み取りセル範囲の指定は必須
+        """
         self.parser.add_argument('template', help='使用するjinja2テンプレート.')
         # source book can't read from stdin
         self.parser.add_argument('source', help='レンダリング対象ブック xlsxファイルのみ対象.')
@@ -31,25 +36,21 @@ class ExcelCommand(Command):
         self.parser.add_argument('read_range', help=ExcelCommand.HELP_READ_RANGE)
 
     def add_optional_arguments(self):
+        """excel固有のオプションを追加する"""
+        # 基本的なオプションは引き継ぐ
         super().add_optional_arguments()
+        # 絶対位置指定セルを追加する
         self.parser.add_argument(
             '-a', '--absolute', help='絶対位置指定でセル値を固定で取得する [セル位置=名前]の形式で列挙する. ex: A1=NAME1 A2=NAME2...', dest='absolute', nargs='*', default={}, action=KeyValuesParseAction)
 
     def call_render(self, *, render, source, out):
         """
-        openpyxlがxlsxファイルのストリームを開けないので、in側はファイル名だけを扱う
+        in側はopenpyxlにファイル名を直接渡す必要があるため、call_renderをoverrideする
         """
-        context = render.context
-        out_stream = sys.stdout
-        try:
-            if out is not sys.stdout:
-                out_stream = open(
-                    out, encoding=context.output_encoding, mode='w')
-
-            render.render(source=source, output=out_stream)
-        finally:
-            if out is not sys.stdout:
-                out_stream.close()
+        with StreamWrapper(useof=out, 
+                           encoding=render.context.output_encoding, 
+                           mode='w' ) as dest:
+            render.render(source=source, output=dest)
 
     def merge_keys(self):
         """設定ファイルとコマンドラインをマージすべき項目名を返す"""
